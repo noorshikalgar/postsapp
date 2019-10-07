ec75b1dd8a3466281137fe5206697bbb61fc5493
noorshikalgar <noor.shikalgar03@gmail.com>
2019-10-08 03:00:29 +0530

    Updates

---
+++ precache-manifest.3d372b27f566c7f48b29a80ab5eeb6c4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cc28efc2f5f5350a479c28e0443a2b12",
+    "url": "/postsapp/index.html"
+  },
+  {
+    "revision": "535cccabcb627782344d",
+    "url": "/postsapp/static/css/2.0624914a.chunk.css"
+  },
+  {
+    "revision": "3ca66ad62b6e4318c9a4",
+    "url": "/postsapp/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "535cccabcb627782344d",
+    "url": "/postsapp/static/js/2.48c72aa1.chunk.js"
+  },
+  {
+    "revision": "3ca66ad62b6e4318c9a4",
+    "url": "/postsapp/static/js/main.e9c745f2.chunk.js"
+  },
+  {
+    "revision": "4d263ba1677d744a9e00",
+    "url": "/postsapp/static/js/runtime-main.7751c6cd.js"
+  }
+]);
